ab2979056fabad44e842b9585fa7eb12ebbe4bdf
Juan C. López-Tavera <jc.lopeztavera@gmail.com>
2017-02-23 23:30:08 -0600

    Updated wrap script

---
+++ R/run_analysis.R
@@ -0,0 +1,6 @@
+# Getting and Cleaning Data Course Project
+# Part 4: Running all the analyses and knitting codebook
+# This code is licensed under a MIT License Copyright (c) 2017 Juan C. López-Tavera
+
+source('~/github/human-activity/R/data-gette.R')
+source('~/github/human-activity/R/data-cleane.R')
