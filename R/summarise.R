7d797e39192d321470b3f6b35810a43ea767b8b6
Juan C. López-Tavera <jc.lopeztavera@gmail.com>
2017-03-06 01:38:56 -0600

    better summaries

--- R/summarise.R
+++ R/summarise.R
@@ -2,16 +2,26 @@
 # Part 3: Summarizing and describing the tidy data
 # This code is licensed under a MIT License Copyright (c) 2017 Juan C. López-Tavera
 
-source('~/github/human-activity/R/data-cleane.R')
+## loading the tidy data
+library(data.table)
+tidy_data <- fread("~/github/human-activity/data/tidy/tidy_data.csv")
 
-## Independent tidy data set with the average of each variable for each activity and each subject.
+## From the data set in the previous step/script, create a second, independent tidy data set with the average of each variable for each activity and each subject.
+by_activity <- tidy_data %>%
+    group_by(activity, acceleration, domain) %>%
+    filter(parameter == "mean", axis == "") %>%
+    summarise(average = mean(value, na.rm = TRUE))
 
-by_activity <- tidy_data %>% melt(id.vars = c("subject", "activity")) %>% group_by(activity) %>% summarise(average = mean(value))
-by_subject <- tidy_data %>% melt(id.vars = c("subject", "activity")) %>% group_by(subject) %>% summarise(average = mean(value))
+by_subject <- tidy_data %>%
+    group_by(subject, acceleration, domain) %>%
+    filter(parameter == "mean", axis == "") %>%
+    summarise(average = mean(value, na.rm = TRUE))
 
 ### Exporting to csv
-write_csv(x = by_activity, path = "~/github/human-activity/data/tidy/by_activity.csv", col_names = TRUE)
-write_csv(x = by_subject, path = "~/github/human-activity/data/tidy/by_subject.csv", col_names = TRUE)
+write_csv(x = by_activity,
+          path = "~/github/human-activity/data/tidy/by_activity.csv",
+          col_names = TRUE)
 
-
-
+write_csv(x = by_subject,
+          path = "~/github/human-activity/data/tidy/by_subject.csv",
+          col_names = TRUE)
